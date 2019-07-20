02836178a4ec69a7cfd211fde0f2736b24e0b877
Sarah Crank <sarahcrank88@gmail.com>
2019-07-20 17:14:03 +0000

    Lesson 4-16: Editing and Updating Grams (there were some errors starting here in case they continue)

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,18 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  
+    @gram.update_attributes(gram_params)
+  
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
 
   def new
     @gram = Gram.new
@@ -7,14 +20,17 @@ class GramsController < ApplicationController
 
   def index
   end
-  
+
   def show
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render plain: 'Not Found :(', status: :not_found
-    end  
+    return render_not_found if @gram.blank?
   end
-  
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
@@ -23,11 +39,14 @@ class GramsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
   end
-  
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
+  end
 end
